38dcc0499837c930023b87a048f0a10114b4da2f
Farhaad Ahmad <farhaadkhan458@gmail.com>
2026-04-16 10:51:39 +0530

    experiments folder

---
+++ Javascript/array.js
@@ -0,0 +1,20 @@
+// let person = {
+//     name : "farhaad",
+//     age : 18,
+//     city : "Geziabad"
+// };
+// console.log(person.name);
+
+// let mixedArray = [1 , "hello", true , {name : "farhaad"}, [1,2,3]];
+
+// console.log(mixedArray[0]);
+// console.log(mixedArray[1]);
+// console.log(mixedArray[2]);
+// console.log(mixedArray[3]);
+// console.log(mixedArray[4]);
+
+let array = [1,2,3,4,5,6];
+let sqarray = array.map(num => num*num);
+console.log(sqarray);
+
+
